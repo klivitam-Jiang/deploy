3cce3838e39e07f1be62d5c2b588e4ee5b7783ae
jiangkunbo <klivitam@outlook.com>
2020-03-29 21:46:28 +0800

    test01

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,46 @@
-var router = require('koa-router')();
+let router = require('koa-router')();
 
-router.get('/', function *(next) {
-  yield this.render('index', {
-    title: 'Hello World Koa!'
-  });
+let crypto = require("crypto");
+let SECRET = "jkb534726781"
+
+const sign = (body) => {
+    return `sha1=${crypto.createHmac("sha1", SECRET).update(body).digest("hex")}`
+}
+
+
+router.get('/', function* (next) {
+    yield this.render('index', {
+        title: 'Hello World Koa!'
+    });
 });
 
-router.get('/foo', function *(next) {
-  yield this.render('index', {
-    title: 'Hello World foo!'
-  });
+router.get('/foo', function* (next) {
+    yield this.render('index', {
+        title: 'Hello World foo!'
+    });
 });
 
+router.post("/deploy", function(next) {
+    let body = this.body;
+    console.log("==============body")
+    console.log("body",body);
+    console.log("==============header")
+    console.log("request",this.request.headers)
+    console.log("==============header")
+})
 
-router.get("/deploy",function *(next) {
-  console.log("lailema?")
-  yield this.render('index', {
-    title: '来了吗？!'
-  });
+
+router.get("/deploy", function* (next) {
+    console.log("==============header")
+    console.log("header:", this.request.headers)
+    console.log("==============header")
+    console.log("=============request")
+    console.log(this.request)
+    console.log("==========request end")
+
+    yield this.render('index', {
+        title: '来了吗？1!'
+    });
 
 })
 
